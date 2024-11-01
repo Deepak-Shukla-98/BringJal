881634df5e144aefdf44de42aa98fcd6f5897033
Deepak-Shukla-98 <112143538+Deepak-Shukla-98@users.noreply.github.com>
2024-11-02 02:10:36 +0530

    deployed on aws lambda

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-const express = require('express');
-const cors = require('cors')
 const dotenv = require('dotenv')
-const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
-const inventoryRoutes = require('./routes/inventoryRoutes');
-const shortest_path = require('./routes/pathRoutes')
+const app = require('./app')
 
 dotenv.config()
 
-const app = express();
+
 const PORT = process.env.PORT || 3000;
-
-connectDB();
-app.use(cors())
-app.use(express.json());
-
-app.use('/api/users', userRoutes);
-app.use('/api/inventories', inventoryRoutes);
-app.use('/api/path', shortest_path);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
